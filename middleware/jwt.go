f5237687e2ac7183bfb1d15aec24e2fbf26cc67f
Administrator <admin@example.com>
2023-09-12 17:33:48 +0800

    update

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -1,11 +1,25 @@
 package middleware
 
 import (
- "github.com/golang-jwt/jwt"
-"github.com/gin-gonic/gin"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 )
 
+// MyClaims 自定义声明结构体并内嵌jwt.StandardClaims
+// jwt包自带的jwt.StandardClaims只包含了官方字段
+// 我们这里需要额外记录一个username字段，所以要自定义结构体
+// 如果想要保存更多信息，都可以添加到这个结构体中
+type MyClaims struct {
+	UserID   int    `json:"id"`
+	Username string `json:"name"`
+	jwt.StandardClaims
+}
 
+const TokenExpireDuration = time.Hour * 1
+
+// https://www.jianshu.com/p/f75d204ce371
 func JwtAuth() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := c.Request.Header.Get(constant.JWTHeaderName)
